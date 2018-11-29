c38ca1ed4182c8c6442c7f81bad37fcb4a39dfc8
Kevin Samoei <samoeikev@gmail.com>
2018-11-29 11:49:02 +0300

    Design login

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="login-image">
+    <div class="french-rose">
+      <div>
+        <p class="andela-media">Andela Media</p>
+        <div>
+          <button class="login-btn">LOGIN WITH GOOGLE</button>
+        </div>
+        <div>
+          <img src="../../assets/andela.png">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+<style scoped>
+html {
+  margin: 0;
+  overflow-y: hidden;
+}
+body::-webkit-scrollbar {
+  display: none;
+}
+.login-image {
+  background: url("../../assets/home.jpg") no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
+.andela-media {
+  height: 79px;
+  width: 370px;
+  color: #ffffff;
+  font-family: DINPro;
+  font-size: 56px;
+  letter-spacing: 1.63px;
+  line-height: 72px;
+}
+.french-rose {
+  height: 750px;
+  width: 1232px;
+  /* background: linear-gradient(157.35deg, #e64c4c 0%, #ef32fb 100%); */
+  background: linear-gradient(
+    157.35deg,
+    rgba(255, 80, 80, 0.8),
+    rgba(239, 50, 251, 0)
+  );
+  margin: 120px 178px 124px 204px;
+}
+.login-btn {
+  height: 52px;
+  width: 300px;
+  border-radius: 8px;
+  background-color: #f44336;
+  color: #ffffff;
+  font-family: DINPro;
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 21px;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import VueAxios from 'vue-axios'
-import axios from './backend/axios'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import VueAxios from 'vue-axios';
+import App from './App';
+import router from './router';
+import axios from './backend/axios';
 
-Vue.config.productionTip = false
-Vue.use(VueAxios, {axios: axios})
+Vue.config.productionTip = false;
+Vue.use(VueAxios, { axios: axios });
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+});
 
 /* eslint-disable no-new */
 new Vue({
@@ -15,5 +21,5 @@ new Vue({
   router,
   axios,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+});
